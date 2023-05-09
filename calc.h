05a0744c583cf3bdc3f610d6a107e9e3316b05cf
k022g0011@g.neec.ac.jp <k022g0011@g.neec.ac.jp>
2023-05-10 02:51:10 +0900

    プロジェクト ファイルを追加します。

---
+++ calc.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include"MyVector2.h"
+#include"MyVector3.h"
+
+class calc
+{
+public:
+	//正規化
+	MyVector2 Normalize(const MyVector2& v);
+	MyVector3 Normalize(const MyVector3& v);
+
+	//内積
+	float Dot(const MyVector2& v1, const MyVector2& v2);
+	float Dot(const MyVector3& v1, const MyVector3& v2);
+
+	//外積
+	
+};
+
---
+++ main.cpp
@@ -0,0 +1,65 @@
+#include <Novice.h>
+#include"MyMatrix4x4.h"
+#include"MatrixScreenPrintf.h"
+#include"MyVector3.h"
+
+const char kWindowTitle[] = "学籍番号";
+
+static const int kRowHeight = 20;
+static const int kColumnWidth = 60;
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR lpCmdLine, _In_ int nShowCmd) {
+
+	// ライブラリの初期化
+	Novice::Initialize(kWindowTitle, 1280, 720);
+
+	// キー入力結果を受け取る箱
+	char keys[256] = {0};
+	char preKeys[256] = {0};
+
+	MyVector3 scale{ 1.2f,0.79f,-2.1f };
+	MyVector3 rotate{ 0.4f,1.43f,-0.8f };
+	MyVector3 translate{ 2.7f,-4.15f,1.57f };
+	MyMatrix4x4 worldMatrix = MyMatrix4x4::MakeAffinMatrix(scale, rotate, translate, MyMatrix4x4::kYZX);
+
+	// ウィンドウの×ボタンが押されるまでループ
+	while (Novice::ProcessMessage() == 0) {
+		// フレームの開始
+		Novice::BeginFrame();
+
+		// キー入力を受け取る
+		memcpy(preKeys, keys, 256);
+		Novice::GetHitKeyStateAll(keys);
+
+		///
+		/// ↓更新処理ここから
+		///
+
+		///
+		/// ↑更新処理ここまで
+		///
+
+		///
+		/// ↓描画処理ここから
+		///
+		
+		MatrixScreenPrintf(0, 0, worldMatrix, "worldMatrix");
+
+		///
+		/// ↑描画処理ここまで
+		///
+
+		// フレームの終了
+		Novice::EndFrame();
+
+		// ESCキーが押されたらループを抜ける
+		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
+			break;
+		}
+	}
+
+	// ライブラリの終了
+	Novice::Finalize();
+	return 0;
+}
